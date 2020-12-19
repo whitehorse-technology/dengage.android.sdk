374069e61efe39350983b40c913be4f56d0ae9c5
Ramazan Dönmez <ramazan@whitehorse.technology>
2020-12-19 15:19:12 +0300

    remove res

--- dEngageSDK/app/src/main/java/com/dengage/sdk/MainActivity.java
+++ dEngageSDK/app/src/main/java/com/dengage/sdk/MainActivity.java
@@ -8,13 +8,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        Context context = getApplicationContext();
-        final DengageManager manager = DengageManager .getInstance(context)
-                .setLogStatus(true)
-                .setFirebaseIntegrationKey("YOUR_INTEGRATION_KEY")
-                .setHuaweiIntegrationKey("YOUR_INTEGRATION_KEY")
-                .init();
+        //Context context = getApplicationContext();
+        //final DengageManager manager = DengageManager .getInstance(context)
+        //        .setLogStatus(true)
+        //        .setFirebaseIntegrationKey("YOUR_INTEGRATION_KEY")
+        //        .setHuaweiIntegrationKey("YOUR_INTEGRATION_KEY")
+        //        .init();
     }
 }
