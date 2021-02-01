c3f70ae506be102eca0a7b6d93664a2616db0a48
Batuhan Coşkun <batu548@gmail.com>
2021-02-02 01:08:34 +0300

    Add json adapter for inbox message data

--- dEngageSDK/app/src/main/java/com/dengage/sdk/models/InboxMessage.kt
+++ dEngageSDK/app/src/main/java/com/dengage/sdk/models/InboxMessage.kt
@@ -1,9 +1,14 @@
 package com.dengage.sdk.models
 
+import com.dengage.sdk.service.InboxMessageDataJsonAdapter
+import com.google.gson.annotations.JsonAdapter
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 /**
  * Created by Batuhan Coskun on 30 November 2020
  */
-class InboxMessage(val id: String, var isClicked: Boolean,
+class InboxMessage(@SerializedName("smsg_id") val id: String,
+                   @SerializedName("is_clicked") var isClicked: Boolean,
+                   @SerializedName("message_json") @JsonAdapter(InboxMessageDataJsonAdapter::class)
                    val data: InboxMessageData) : Serializable
