6a3fe1c5d6996080df8eb5bd81e44cf8a1a48166
Batuhan Coşkun <batu548@gmail.com>
2021-02-19 00:53:58 +0300

    Update prefs tests

--- dEngageSDK/app/src/androidTest/java/com/dengage/sdk/cache/PrefsTest.kt
+++ dEngageSDK/app/src/androidTest/java/com/dengage/sdk/cache/PrefsTest.kt
@@ -2,13 +2,11 @@ package com.dengage.sdk.cache
 
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
-import com.dengage.sdk.inappmessage.model.*
-import com.dengage.sdk.models.InboxMessage
+import com.dengage.sdk.models.SdkParameters
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import java.util.*
 
 /**
  * Created by Batuhan Coskun on 27 December 2020
@@ -30,18 +28,53 @@ class PrefsTest {
     }
 
     @Test
+    fun saveSdkParametersToPrefsTest() {
+        val accountId = 1
+        val accountName = "accountName"
+        val eventsEnabled = true
+        val inboxEnabled = true
+        val inAppEnabled = true
+        val subscriptionEnabled = true
+        val lastFetchTimeInMillis = 10L
+
+        val sdkParameters = SdkParameters(
+                accountId = accountId,
+                accountName = accountName,
+                eventsEnabled = eventsEnabled,
+                inboxEnabled = inboxEnabled,
+                inAppEnabled = inAppEnabled,
+                subscriptionEnabled = subscriptionEnabled,
+                lastFetchTimeInMillis = lastFetchTimeInMillis
+        )
+        prefs.sdkParameters = sdkParameters
+
+        Assert.assertEquals(prefs.sdkParameters?.accountId, accountId)
+        Assert.assertEquals(prefs.sdkParameters?.accountName, accountName)
+        Assert.assertEquals(prefs.sdkParameters?.eventsEnabled, eventsEnabled)
+        Assert.assertEquals(prefs.sdkParameters?.inboxEnabled, inboxEnabled)
+        Assert.assertEquals(prefs.sdkParameters?.inAppEnabled, inAppEnabled)
+        Assert.assertEquals(prefs.sdkParameters?.subscriptionEnabled, subscriptionEnabled)
+        Assert.assertEquals(prefs.sdkParameters?.lastFetchTimeInMillis, lastFetchTimeInMillis)
+    }
+
+    @Test
     fun saveSessionNavigationCountToPrefsTest() {
         val sessionNavigationCount = 2
         prefs.sessionNavigationCount = sessionNavigationCount
-
         Assert.assertEquals(prefs.sessionNavigationCount, sessionNavigationCount)
+    }
+
+    @Test
+    fun saveInAppMessageFetchTimeToPrefsTest() {
+        val time = 20L
+        prefs.inAppMessageFetchTime = time
+        Assert.assertEquals(prefs.inAppMessageFetchTime, time)
     }
 
     @Test
     fun clearPrefsTest() {
         prefs.sessionNavigationCount = 1
         prefs.clear()
-
         Assert.assertEquals(prefs.sessionNavigationCount, 0)
     }
 
