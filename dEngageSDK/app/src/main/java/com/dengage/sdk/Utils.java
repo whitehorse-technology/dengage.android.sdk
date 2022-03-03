e55fb043d030c01b1a5a1dbab6dd8bc127c99367
Dengage <57666388+whitehorse-technology@users.noreply.github.com>
2022-03-03 13:30:38 +0500

    Update Utils.java

--- dEngageSDK/app/src/main/java/com/dengage/sdk/Utils.java
+++ dEngageSDK/app/src/main/java/com/dengage/sdk/Utils.java
@@ -82,7 +82,7 @@ public class Utils {
     }
 
     public static String getSdkVersion(Context context) {
-        return "4.3.0";
+        return "4.3.1";
     }
 
     public static String getOsVersion() {
